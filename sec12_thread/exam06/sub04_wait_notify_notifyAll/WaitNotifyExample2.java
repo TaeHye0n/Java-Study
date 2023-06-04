4b5b64f5bdb834e97fe8209b562b25a53f45eca7
TaeHye0n <thk001229@gmail.com>
2023-06-04 19:53:59 +0900

    add exam06.sub04

---
+++ sec12_thread/exam06/sub04_wait_notify_notifyAll/WaitNotifyExample2.java
@@ -0,0 +1,2 @@
+package sec12_thread.exam06.sub04_wait_notify_notifyAll;public class WaitNotifyExample2 {
+}
---
+++ sec12_thread/exam06/sub04_wait_notify_notifyAll/WorkObject.java
@@ -0,0 +1,4 @@
+package sec12_thread.exam06.sub04_wait_notify_notifyAll;
+
+public class WorkObject {
+}
