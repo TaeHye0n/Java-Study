dbc5364ce7c19f8486f8bc59c11cbd1478d1f4e8
TaeHye0n <thk001229@gmail.com>
2023-06-05 18:40:28 +0900

    add prob

---
+++ sec13_generic/problems/prob04/Util.java
@@ -0,0 +1,8 @@
+package sec13_generic.problems.prob04;
+
+public class Util {
+    public static <K, V> V getValue(Pair<K, V> p1, K k){
+        if(p1.getKey() == k) return p1.getValue();
+        else return null;
+    }
+}
